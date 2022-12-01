7fccaf79ac39c03ce97e5b924e20253a57c3126d
EthanDMello <107923914+EthanDMello@users.noreply.github.com>
2022-12-01 14:58:56 +0000

    files copied

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const db = require('./config/connection');
-const routes = require('./routes');
-
-const cwd = process.cwd();
+const express = require("express");
+const mongodb = require("mongodb").MongoClient;
+const routes = require("./routes");
 
 const PORT = process.env.port || 3001;
 const app = express();
-
-// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
-const activity = cwd.includes('01-Activities')
-  ? cwd.split('/01-Activities/')[1]
-  : cwd;
+const connectionString = `mongodb://127.0.0.1:27017/socialNetwork_db`;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server for ${activity} running on port ${PORT}!`);
-  });
-});
+let db;
+
+mongodb.connect(
+  connectionString,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, client) => {
+    db = client.db();
+    app.listen(PORT, () => {
+      console.log(`Example app listening at http://localhost:${PORT}`);
+    });
+  }
+);
