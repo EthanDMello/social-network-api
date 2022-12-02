1e1a56eea2111cc633eaa3f669d825d792bc5439
EthanDMello <107923914+EthanDMello@users.noreply.github.com>
2022-12-02 13:27:26 +0000

    user virtual

--- models/User.js
+++ models/User.js
@@ -36,6 +36,19 @@ const userSchema = new Schema(
   }
 );
 
+userSchema
+  .virtual("fr")
+  // Getter
+  .get(function () {
+    return `${this.first} ${this.last}`;
+  })
+  // Setter to set the first and last name
+  .set(function (v) {
+    const first = v.split(" ")[0];
+    const last = v.split(" ")[1];
+    this.set({ first, last });
+  });
+
 const User = model("user", userSchema);
 
 module.exports = User;
