f5451d8d04fe938d56201d78871cf5a9b086e246
EthanDMello <107923914+EthanDMello@users.noreply.github.com>
2022-12-03 19:13:59 +0000

    Thoughts schema work

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -7,18 +7,13 @@ const thoughtSchema = new Schema(
     // Configure individual properties using Schema Types
     thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
     createdAt: {
-      type: String,
-      unique: true,
-      required: true,
-      match: [
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-        "Please fill a valid email address",
-      ],
+      type: Date,
+      default: Date.now,
     },
-    thoughts: [
+    username: [
       {
         type: Schema.Types.ObjectId,
-        ref: "Thoughts",
+        ref: "User",
       },
     ],
   },
